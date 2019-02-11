b5e050686aafcc11c6a50ad3d12ac3ac48f89ba4
Aravindh Sridaran <mail@aravindh.io>
2019-02-11 20:44:00 +0100

    Successfully added dic to dom

--- src/Main.purs
+++ src/Main.purs
@@ -1,15 +1,21 @@
 module Main where
   
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromJust)
 import Data.Tuple (Tuple)
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (Unit, bind, pure, (>>>), (<<<))
-import Web.DOM.Document (Document, createElement)
+import Partial.Unsafe (unsafePartial)
+import Prelude (Unit, bind, pure, (>>>), (<<<), unit)
+import Web.DOM.Document (Document, createElement, toNonElementParentNode)
 import Web.DOM.Internal.Types (Element)
-import Web.HTML (HTMLDocument, Window, window)
-import Web.HTML.HTMLDocument (toDocument)
+import Web.DOM.Node (appendChild)
+import Web.DOM.NonElementParentNode (NonElementParentNode, getElementById)
+import Web.HTML (HTMLDocument, HTMLElement, Window, window)
+import Web.HTML.HTMLDocument (body, toDocument)
+import Web.HTML.HTMLElement (fromElement, toNode)
 import Web.HTML.Window (document)
+
+
 data Model a = Model a
 
 data Event a = Event a
@@ -34,21 +40,29 @@ data Node = Node {
 generateNode :: forall model event. App model event -> Node
 generateNode app = Node { attributes: [], type:"div", children: []}
 
-render :: Node -> Effect Element
+render :: Node -> Effect Unit
 render node = do
-    currentWindow <- window :: Effect Window
+    currentWindow :: Window <- window :: Effect Window
     currentHTMLDocument <- document currentWindow :: Effect HTMLDocument
-    let currentDocument = toDocument currentHTMLDocument :: Document
-    createElement "div" currentDocument
+    let doc = toDocument currentHTMLDocument
+    createdElement <- createElement "div" doc
+    let nepn = toNonElementParentNode doc :: NonElementParentNode
+    mayBeRoot <- (getElementById  "main" nepn) :: Effect (Maybe Element )
+    let root =  (unsafePartial fromJust mayBeRoot) :: Element
+    _ <- log "adding element"
+    let rootNode = toNode (unsafePartial fromJust (fromElement root))
+    _ <- appendChild (toNode (unsafePartial fromJust (fromElement createdElement))) rootNode
+    pure unit
 
-runApp :: forall model event. App model event -> model -> Effect  Element
+
+runApp :: forall model event. App model event -> model -> Effect  Unit
 runApp app model = render (generateNode app)
     
 data App model event =  App { 
         rootComponent :: Component model event
     }
 
-main :: Effect Element
+main :: Effect Unit
 main = do 
     let rootComponent = Component { init: 0 , view: (\x -> ""), update: (\model -> \e -> model)}
     let app =  App { rootComponent: rootComponent}
