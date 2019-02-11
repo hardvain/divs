e1c319db6480b8c1acb5e4f3c6a46d0b0ffd3d37
Aravindh Sridaran <mail@aravindh.io>
2019-02-11 20:46:07 +0100

    Code restructuring

---
+++ src/HTML.purs
@@ -0,0 +1,40 @@
+module HTML where
+ 
+import Data.Maybe (Maybe, fromJust)
+import Data.Tuple (Tuple)
+import Effect (Effect)
+import Effect.Console (log)
+import Partial.Unsafe (unsafePartial)
+import Prelude (Unit, bind, pure, unit)
+import Web.DOM.Document (createElement, toNonElementParentNode)
+import Web.DOM.Internal.Types (Element)
+import Web.DOM.Node (appendChild)
+import Web.DOM.NonElementParentNode (NonElementParentNode, getElementById)
+import Web.HTML (HTMLDocument, Window, window)
+import Web.HTML.HTMLDocument (toDocument)
+import Web.HTML.HTMLElement (fromElement, toNode)
+import Web.HTML.Window (document)
+
+type Attribute = Tuple String String
+
+data Node = Node {
+    attributes :: Array Attribute,
+    type :: String,
+    children :: Array Node
+}
+
+
+render :: Node -> Effect Unit
+render node = do
+    currentWindow :: Window <- window :: Effect Window
+    currentHTMLDocument <- document currentWindow :: Effect HTMLDocument
+    let doc = toDocument currentHTMLDocument
+    createdElement <- createElement "div" doc
+    let nepn = toNonElementParentNode doc :: NonElementParentNode
+    mayBeRoot <- (getElementById  "main" nepn) :: Effect (Maybe Element )
+    let root =  (unsafePartial fromJust mayBeRoot) :: Element
+    _ <- log "adding element"
+    let rootNode = toNode (unsafePartial fromJust (fromElement root))
+    _ <- appendChild (toNode (unsafePartial fromJust (fromElement createdElement))) rootNode
+    pure unit
+
--- src/Main.purs
+++ src/Main.purs
@@ -1,19 +1,8 @@
 module Main where
   
-import Data.Maybe (Maybe(..), fromJust)
-import Data.Tuple (Tuple)
 import Effect (Effect)
-import Effect.Console (log)
-import Partial.Unsafe (unsafePartial)
-import Prelude (Unit, bind, pure, (>>>), (<<<), unit)
-import Web.DOM.Document (Document, createElement, toNonElementParentNode)
-import Web.DOM.Internal.Types (Element)
-import Web.DOM.Node (appendChild)
-import Web.DOM.NonElementParentNode (NonElementParentNode, getElementById)
-import Web.HTML (HTMLDocument, HTMLElement, Window, window)
-import Web.HTML.HTMLDocument (body, toDocument)
-import Web.HTML.HTMLElement (fromElement, toNode)
-import Web.HTML.Window (document)
+import Prelude (Unit)
+import HTML
 
 
 data Model a = Model a
@@ -29,31 +18,10 @@ data Component model event = Component {
     }
 
 type HTML = String
-type Attribute = Tuple String String
 type DOM = String
-data Node = Node {
-    attributes :: Array Attribute,
-    type :: String,
-    children :: Array Node
-}
 
 generateNode :: forall model event. App model event -> Node
 generateNode app = Node { attributes: [], type:"div", children: []}
-
-render :: Node -> Effect Unit
-render node = do
-    currentWindow :: Window <- window :: Effect Window
-    currentHTMLDocument <- document currentWindow :: Effect HTMLDocument
-    let doc = toDocument currentHTMLDocument
-    createdElement <- createElement "div" doc
-    let nepn = toNonElementParentNode doc :: NonElementParentNode
-    mayBeRoot <- (getElementById  "main" nepn) :: Effect (Maybe Element )
-    let root =  (unsafePartial fromJust mayBeRoot) :: Element
-    _ <- log "adding element"
-    let rootNode = toNode (unsafePartial fromJust (fromElement root))
-    _ <- appendChild (toNode (unsafePartial fromJust (fromElement createdElement))) rootNode
-    pure unit
-
 
 runApp :: forall model event. App model event -> model -> Effect  Unit
 runApp app model = render (generateNode app)
